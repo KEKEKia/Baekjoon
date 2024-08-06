3f6aecf0e3607ff8583498b05adca45429bc69d8
Lee 정석 <jungsuk7083@gmail.com>
2024-08-06 11:56:04 +0900

    [Silver II] Title: 쇠막대기, Time: 116 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,28 +1,39 @@
 import java.util.*;
 import java.io.*;
 
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+//		System.out.println(str.charAt(0));
+		
+		
+		Stack<Character> stack  = new Stack<>();
 
-        Stack<Character> stack = new Stack<>();
-        int ans = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            char now = str.charAt(i);
-            
-            if (now == '(') {
-                stack.push(now);
-            } else {
-                stack.pop();
-                if (str.charAt(i - 1) == '(') {
-                    ans += stack.size();
-                } else {
-                    ans += 1;
-                }
-            }
-        }
-        System.out.println(ans);
-    }
+		int cnt = 0 ; 
+		int ans = 0 ;
+		char last = 0;
+		
+		for(int i = 0 ; i< str.length(); i++) {
+			char now = str.charAt(i);
+			
+			if(now == '(') {
+				stack.push(now);
+				cnt++;
+			}else if( now ==')'&& last == ')') { //닫히는 쇠막대기일때 
+				stack.pop();
+				ans++;
+				cnt--;
+			}else { //레이저일 때
+				stack.pop();
+				cnt--;
+				ans+=cnt;
+			}
+			last = now;
+			
+		}
+		System.out.println(ans);
+		
+	}
 }
