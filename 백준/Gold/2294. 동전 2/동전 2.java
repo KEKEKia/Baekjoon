85540c882fc0627633c8ea65b85640b397f69d27
Lee 정석 <jungsuk7083@gmail.com>
2024-08-07 14:58:29 +0900

    [Gold V] Title: 동전 2, Time: 112 ms, Memory: 13300 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class Main {
 	
 	public static void main(String[] args) {
@@ -9,27 +10,36 @@ public class Main {
 		
 		int[] arr = new int[N];
 		for(int n = 0 ; n < N; n++) {
-			arr[n] = sc.nextInt();
+			arr[n]=sc.nextInt();
 		}
 		
 		Arrays.sort(arr);
+//		System.out.println(Arrays.toString(arr));
 		
 		int[] dp = new int[K+1];
 		Arrays.fill(dp, Integer.MAX_VALUE);
-		dp[0] = 0; // dp 배열 초기값 설정
+//		for(int n =0; n<N; n++) {
+//			if(arr[n]<=K)
+//			dp[arr[n]]=1;
+//		}
+		dp[0]=0;
 		
-		for(int k = 1; k <= K; k++) {
-			for(int n = 0; n < N; n++) {
-				if (k - arr[n] >= 0 && dp[k - arr[n]] != Integer.MAX_VALUE) {
-					dp[k] = Math.min(dp[k - arr[n]] + 1, dp[k]);
+		for(int k = 1 ; k<= K; k++) {
+			for(int n=0; n< N;n++) {
+				if(k-arr[n]>=0&& dp[k-arr[n]] != Integer.MAX_VALUE) {
+					dp[k] = Math.min(dp[k-arr[n]]+1,dp[k]);
 				}
 			}
 		}
+//		System.out.println(Arrays.toString(dp));
 		
-		if(dp[K] == Integer.MAX_VALUE) {
+		if(dp[K]==Integer.MAX_VALUE) {
 			System.out.println(-1);
-		} else {
+		}else {
 			System.out.println(dp[K]);
 		}
+		
+		
+		
 	}
 }
