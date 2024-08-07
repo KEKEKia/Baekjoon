dd2c93da0a3302ec0ffbb88dd039ce5a868b6d48
Lee 정석 <jungsuk7083@gmail.com>
2024-08-07 16:27:14 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 13360 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,60 +1,47 @@
 import java.util.*;
 
-
 public class Main {
-	
-	static ArrayList<Integer>[] graph;
-	static boolean[] visited;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int K = sc.nextInt();
-		
-		
-		graph = new ArrayList[N+1];
-		visited = new boolean[N+1];
-		
-		for(int n = 1; n<= N; n++) {
-			graph[n]=new ArrayList<>();
-		}
-		
-		for(int n =0; n<K; n++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			graph[a].add(b);
-			graph[b].add(a);
-		}
-		bfs(1);
-		
-		int ans = 0;
-		for(int n = 1; n<= N; n++) {
-			if(visited[n]) ans++;
-			
-		}
-		
-		System.out.println(ans-1);
-	}
-	
-	
-	static void bfs(int n) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(1);
-		visited[1]=true;
-		while(!queue.isEmpty()) {
-			int tmp = queue.poll();
-			for(int i = 0 ; i < graph[tmp].size();i++) {
-				if(visited[graph[tmp].get(i)]) continue;
-				queue.add(graph[tmp].get(i));
-				visited[graph[tmp].get(i)]=true;
-			}
-		}
-		
-		
-		
-		
-		
-	}
-	
+
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int n = 1; n <= N; n++) {
+            graph[n] = new ArrayList<>();
+        }
+
+        for (int n = 0; n < K; n++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        dfs(1);
+
+        int ans = 0;
+        for (int n = 1; n <= N; n++) {
+            if (visited[n]) ans++;
+        }
+
+        System.out.println(ans - 1);
+    }
+
+    static void dfs(int n) {
+        visited[n] = true; // 현재 노드를 방문으로 표시
+
+        for (int i = 0; i < graph[n].size(); i++) {
+            int next = graph[n].get(i);
+            if (!visited[next]) {
+                dfs(next); // 다음 노드로 이동
+            }
+        }
+    }
 }
