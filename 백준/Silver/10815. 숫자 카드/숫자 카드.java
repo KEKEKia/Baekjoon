1d0ce38a22a5ffc24a4f69b09f319e90461ffd78
Lee 정석 <jungsuk7083@gmail.com>
2024-08-12 11:35:21 +0900

    [Silver V] Title: 숫자 카드, Time: 2692 ms, Memory: 180908 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -3,60 +3,75 @@ import java.io.*;
 
 
 public class Main {
+	static int[] dp;
+	
 	public static void main(String[] args) throws IOException{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
-		
+		int N = Integer.valueOf(str);
+		int[] arrN = new int[N];
+		str = br.readLine();
 		StringTokenizer st = new StringTokenizer(str);
-		int N = Integer.valueOf(st.nextToken());
-		int[] arr = new int[N];
-		str= br.readLine();
-		st = new StringTokenizer(str);
-		for(int n = 0 ; n < N; n++) {
-			arr[n] = Integer.valueOf(st.nextToken());
+		for(int n = 0; n < N; n++) {
+			arrN[n] = Integer.valueOf(st.nextToken());
 		}
+		str = br.readLine();
+		int M = Integer.valueOf(str);
 		str= br.readLine();
 		st = new StringTokenizer(str);
-
-		int M = Integer.valueOf(st.nextToken());
-		str= br.readLine();
-		st = new StringTokenizer(str);
-		int[] testArr = new int[M];
-		for(int m = 0 ; 	m < M; m++) {
-			testArr[m]=Integer.valueOf(st.nextToken());
+		int[] arrM = new int[M];
+		for(int m = 0 ; m < M; m++) {
+			arrM[m] = Integer.valueOf(st.nextToken());
 		}
-//		System.out.println(N);
-		Arrays.sort(arr);
-//		System.out.println(Arrays.toString(arr));
-//		System.out.println(M);
-		for(int m = 0 ; m<M; m++) {
-			
-			int starter = 0;
-			int ender = N-1;
-			int test = testArr[m];
-			boolean found = false;
+		
+//		System.out.println(Arrays.toString(arrM));
+		
+		
+		//완전탐색
+//		for(int m = 0 ; m < M; m++) {
+//			for(int n = 0 ; n < N; n++) {
+//				if(arrM[m]==arrN[n]) {
+//					System.out.print(1 + " ");
+//					break;
+//				}
+//				if(n==N-1) System.out.print(0 + " ");
+//			}
+//		}
+		
+		// 시간 복잡도는 O(N*M) -> 문제 조건에서 최악의 케이스는 2500억개의 연산
+		
+		
+		//이분탐색 while문을 쓰는게 기본
+		Arrays.sort(arrN);
+		for(int m = 0; m<M; m++) {
+			//초기 index
+			int start = 0; 
+			int end = N-1;
 
-			while(starter<=ender) {
-
-				int middler = (starter+ender)/2;
-				
-				if(arr[middler]<test) {
-					starter= middler+1;
-				}else if(arr[middler]>test) {
-					ender = middler-1;
+//			System.out.println("인간디버거 이정석");
+			while(start<=end) {
+				int mid = (start + end)/2;
+				int valueMid = arrN[mid];
+				if(valueMid>arrM[m]) {
+					end = mid-1;
+				}else if (valueMid<arrM[m]) {
+					start = mid+1;
 				}else {
-					found=true;
+					System.out.print(1 + " ");
 					break;
 				}
-				
-				
+				if(start>end) {
+					System.out.print(0 + " ");
+					break;
+				}
 			}
-			if(found) {System.out.print(1+" ");}
-			else {System.out.print(0 +  " ");}
+			
 			
 		}
-		
 		
 		
 	}
+	
+
 }
